379415d1e29661668618e93c53f6409aa14cd7eb
aiharayuriko <g15901ay@gm.tsuda.acjp>
2017-06-20 16:51:49 +0900

    次は背景追加！！

--- app/src/main/java/com/example/yuzukifukushima/battlecollege/Start.java
+++ app/src/main/java/com/example/yuzukifukushima/battlecollege/Start.java
@@ -2,12 +2,30 @@ package com.example.yuzukifukushima.battlecollege;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.app.Activity;
+import android.content.Intent;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Button;
+import android.view.View.OnClickListener;
 
-public class Start extends AppCompatActivity {
+public class Start extends AppCompatActivity implements OnClickListener{
+
+    private Button button_segue;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_start);
+
+        button_segue=(Button)findViewById(R.id.button2);
+        button_segue.setOnClickListener(this);
+
+    }
+    public void onClick(View v){
+        if(v==button_segue){
+            Intent intent = new Intent(this, Room.class);
+            startActivityForResult(intent,0);
+        }
     }
 }
